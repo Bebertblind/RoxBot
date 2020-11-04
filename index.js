26e18bbfb7862f1387a8e9e9bf35c775a5ae7065
Bebertblind <73948746+Bebertblind@users.noreply.github.com>
2020-11-04 15:58:59 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+//Toutes les actions à faire quand le bot se connecte
+client.on("ready", function () {
+    console.log("Mon BOT est Connecté");
+})
+
+client.login("NzcwMzYxMjYyNjk3MzQ5MTQx.X5cc1Q.yI5N5YYHiRK79UNKwi8CE2Q6RTk");
+
+
